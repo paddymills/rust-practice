d383f44585a44d219946c3507b7eb6a474a414bd
paddymills <paddymills@pm.me>
2020-08-04 14:25:28 -0400

    basic case done

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
-// add some imports
+use rand::prelude::*;
 
 fn main() {
-    // add some code
+    const LOW: u8 = 0; // inclusive
+    const HIGH: u8 = 10; // exclusive
+
+    let mut rng = thread_rng();
+    for a in 1..10 {
+        let x = rng.gen_range(LOW, HIGH);
+        println!("iteration {}: {}", a, x);
+    }
 }

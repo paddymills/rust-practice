ce262b2ee173ad7c3db0221c9f6c53fe358c2770
paddymills <paddymills@pm.me>
2020-08-05 11:54:28 -0400

    complete

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,39 @@
+use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::iter::Sum;
+use std::path::PathBuf;
 use std::str::FromStr;
+use structopt::StructOpt;
 
-// cli to use a number file?
+// cli to use a number file
+#[derive(Debug, StructOpt)]
+#[structopt(name = "average", about = "Average number generator")]
+struct Cli {
+    /// Input file
+    #[structopt(parse(from_os_str))]
+    file: Option<PathBuf>,
+}
+
+enum LineResult<T> {
+    Number(T),
+    StopIteration,
+    SkipLine(String),
+}
 
 fn main() {
-    let numbers = get_numbers_from_user::<f64>().unwrap();
-    let sum: f64 = Sum::sum(numbers.iter());
+    type NumType = f64;
+
+    let opt = Cli::from_args();
+    let numbers: Vec<NumType> = match opt.file {
+        Some(f) => get_numbers_from_file::<NumType>(f).unwrap(),
+        None => get_numbers_from_user::<NumType>().unwrap(),
+    };
+
+    let sum: NumType = Sum::sum(numbers.iter());
 
     println!("numbers: {:?}", numbers);
-    println!("average: {}", sum / numbers.len() as f64)
+    println!("average: {}", sum / numbers.len() as NumType)
 }
 
 fn get_numbers_from_user<T: FromStr>() -> io::Result<Vec<T>> {
@@ -20,25 +43,61 @@ fn get_numbers_from_user<T: FromStr>() -> io::Result<Vec<T>> {
         print!("Enter a number: ");
         io::stdout().flush().unwrap();
 
-        let mut input = String::new();
-        match io::stdin().read_line(&mut input) {
-            Ok(_) => {
-                let num = input.trim_end();
-
-                if num == "" {
-                    break;
-                }
-                match num.parse::<T>() {
-                    Ok(n) => numbers.push(n),
-                    Err(_) => println!("Invalid input: {}", num),
-                }
-            }
-            Err(error) => {
-                println!("io read error: {}", error);
-                break;
+        match read_and_parse_number(&mut io::stdin().lock()) {
+            LineResult::Number(n) => numbers.push(n),
+            LineResult::StopIteration => break,
+            LineResult::SkipLine(val) => {
+                println!("Invalid input: {}", val);
+                continue;
             }
         }
     }
 
     Ok(numbers)
+}
+
+fn get_numbers_from_file<T: FromStr>(filepath: PathBuf) -> io::Result<Vec<T>> {
+    let mut numbers: Vec<T> = Vec::new();
+
+    println!("file: {:?}", filepath);
+
+    let f = File::open(filepath)?;
+    let mut reader = io::BufReader::new(f);
+    loop {
+        match read_and_parse_number(&mut reader) {
+            LineResult::Number(n) => numbers.push(n),
+            LineResult::StopIteration => break,
+            LineResult::SkipLine(val) => {
+                println!("Invalid input in file: {}", val);
+                continue;
+            }
+        }
+    }
+
+    Ok(numbers)
+}
+
+fn read_and_parse_number<T: FromStr>(reader: &mut dyn io::BufRead) -> LineResult<T> {
+    let mut line = String::new();
+    match reader.read_line(&mut line) {
+        Ok(0) => LineResult::StopIteration,
+        Ok(_) => {
+            let num = line.trim_end();
+
+            match num.parse::<T>() {
+                Ok(n) => LineResult::Number(n),
+                Err(_) => {
+                    if num == "" {
+                        LineResult::StopIteration
+                    } else {
+                        LineResult::SkipLine(line)
+                    }
+                }
+            }
+        }
+        Err(error) => {
+            println!("io read error: {}", error);
+            LineResult::StopIteration
+        }
+    }
 }

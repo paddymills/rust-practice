2d74d79b97e2fd28b1bc6cc229ee2833e7f8a23e
paddymills <paddymills@pm.me>
2020-08-05 08:16:22 -0400

    parsing complete

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,23 @@
 // add some imports
 
 fn main() {
-    // add some code
+    let test = String::from("ABBEACEEA");
+    let mut k = 0;
+    let len = test.len();
+
+    for mut c in test.chars() {
+        if !c.is_alphabetic() {
+            panic!("Expected alphabetic character, got {}", c);
+        }
+
+        c.make_ascii_uppercase();
+
+        let c_k = c as u32 - 64;
+        if c_k > k {
+            k = c_k;
+        }
+    }
+
+    //should be k=5, len=9
+    println!("{} is a string of k: {} and length: {}", test, k, len);
 }

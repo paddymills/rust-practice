bd76be24a12c8f9b870d02d45d678beffa10831c
Patrick Miller <paddymills@pm.me>
2020-08-09 11:39:41 -0400

    move scripts to main

--- scripts/new.sh
+++
@@ -1,8 +0,0 @@
-if [$1 -eq "" ] 
-then
-  echo "new branch name not supplied"
-else
-  git checkout template
-  git branch $1
-  git checkout $1
-fi
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,30 @@
-// add some imports
+use std::process::Command;
+use structopt::StructOpt;
+
+#[derive(Debug, StructOpt)]
+#[structopt(name = "branch", about = "branch template")]
+struct Opt {
+    name: String,
+}
 
 fn main() {
-    // add some code
+    let opt = Opt::from_args();
+
+    run("git checkout template");
+    run(&("git branch ".to_owned() + opt.name.as_str()));
+    run(&("git checkout ".to_owned() + opt.name.as_str()));
+}
+
+fn run(args: &str) {
+    let (shell, c) = if cfg!(target_os = "windows") {
+        ("cmd", "/C")
+    } else {
+        ("sh", "-c")
+    };
+
+    Command::new(shell)
+        .arg(c)
+        .arg(args)
+        .output()
+        .expect("failed to execute process");
 }

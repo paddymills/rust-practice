349d1a762c226f5a0941877521e31da0810fd592
Patrick Miller <paddymills@pm.me>
2020-09-19 08:24:00 -0400

    input loop needs errors caught

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,40 @@
-// add some imports
+use std::io;
+use std::io::prelude::Write;
+use std::num::IntErrorKind;
 
-fn main() {
-    // add some code
+fn main() -> io::Result<()> {
+    let num = input();
+
+    Ok(())
+}
+
+fn input() -> io::Result<i32> {
+    let mut stdout = io::stdout();
+    let stdin = io::stdin();
+
+    let mut iters = 0;
+    loop {
+        let mut input = String::new();
+        stdout.write(b"n!: n=")?;
+        stdout.flush()?;
+
+        stdin.read_line(&mut input)?;
+        match input.trim().parse::<i32>() {
+            Ok(number) => println!("n= {}", number),
+            Err(e) => {
+                println!("err: {:?}", e);
+                match e.kind() {
+                    IntErrorKind::Empty => println!("Empty string, abort!"),
+                    IntErrorKind::InvalidDigit => println!("not a number"),
+                }
+            }
+        };
+
+        iters += 1;
+        if iters > 2 {
+            break;
+        }
+    }
+
+    Ok(0)
 }
